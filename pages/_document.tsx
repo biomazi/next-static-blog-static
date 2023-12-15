f75c9aa6ef058cc291b60b5f2099145062ee0386
Marko Mazibrada <biomazi@gmail.com>
2023-12-15 20:08:41 +0100

    first commit

---
+++ pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <meta name='Marko i Damir' />
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
